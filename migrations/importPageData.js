9ddb5fd15e7c7f09d896327a7ed3d1c4325ce792
sebbex1337 <sefort@hotmail.dk>
2024-11-15 12:51:03 +0100

    Added current_timestamp

--- migrations/importPageData.js
+++ migrations/importPageData.js
@@ -16,7 +16,6 @@ async function importData() {
       url: page.url,
       title: page.title,
       content: page.content,
-      last_updated: new Date(),
     }));
 
     await knexPostgres("pages").insert(formattedData).onConflict("title", "url").ignore();
